c7a25af3f9bccc6f8492eda2591387edefb2e7ee
Andre Julius <norom@jimileibniz.de>
2018-01-02 02:09:12 +0100

    Add random ascii char generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate rand;
+use rand::{thread_rng, Rng};
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    if let Some(first_argument) = env::args().nth(1) {
+        if let Ok(count) = first_argument.parse::<usize>() {
+            let mut rng = thread_rng();
+            let gen = rng.gen_ascii_chars();
+            let result = gen.take(count);
+            for elem in result {
+                print!("{}", elem);
+            }
+            println!("");
+        } else {
+            println!("Please enter a number > 0.");
+        }
+    }
 }
